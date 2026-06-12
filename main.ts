d714faa6f118b7dc4d6fcda939ce8b5272de4a68
BitStation <121453087+BitStationBusiness@users.noreply.github.com>
2026-06-12 20:47:25 +0200

    y

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-function girar_izquierda (velocidad: number, tiempo_ms: number) {
+function girar_izquierda(velocidad: number, tiempo_ms: number) {
     basic.showLeds(`
         . . # . .
         . . . # .
@@ -8,42 +8,50 @@ function girar_izquierda (velocidad: number, tiempo_ms: number) {
         `)
     girar_temporal(xgo.rotate_enum.Left, velocidad, tiempo_ms)
 }
-// =========================
-// FUNCIONES DE MOVIMIENTO
-// =========================
-function mover_temporal (direccion: number, velocidad: number, tiempo_ms: number) {
-    xgo.move_xgo(direccion, velocidad)
-basic.pause(tiempo_ms)
+
+//  =========================
+//  FUNCIONES DE MOVIMIENTO
+//  =========================
+function mover_temporal(direccion: number, velocidad2: number, tiempo_ms2: number) {
+    xgo.move_xgo(direccion, velocidad2)
+    basic.pause(tiempo_ms2)
     xgo.move_xgo(direccion, 0)
 }
-function detener_movimiento () {
+
+function detener_movimiento() {
     xgo.move_xgo(xgo.direction_enum.Forward, 0)
 }
-function detener_giro () {
+
+function detener_giro() {
     xgo.rotate(xgo.rotate_enum.Left, 0)
 }
-// =========================
-// BOTONES
-// =========================
-input.onButtonPressed(Button.A, function () {
+
+//  =========================
+//  BOTONES
+//  =========================
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     avanzar(100, 1500)
     girar_izquierda(100, 1500)
     girar_derecha(100, 1500)
     retroceder(100, 1500)
 })
-function cerrar_gancho () {
+function cerrar_gancho() {
+    
     clamp_state = 1
     xgo.Manipulator_clamp(gancho_cerrado)
 }
-// =========================
-// FUNCIONES DEL GANCHO
-// =========================
-function abrir_gancho () {
+
+//  =========================
+//  FUNCIONES DEL GANCHO
+//  =========================
+function abrir_gancho() {
+    
     let gancho_abierto = 0
     clamp_state = 0
     xgo.Manipulator_clamp(gancho_abierto)
 }
-function toggle_gancho () {
+
+function toggle_gancho() {
     if (clamp_state == 0) {
         basic.showLeds(`
             . # . # .
@@ -63,8 +71,10 @@ function toggle_gancho () {
             `)
         abrir_gancho()
     }
+    
 }
-function avanzar (velocidad: number, tiempo_ms: number) {
+
+function avanzar(velocidad3: number, tiempo_ms3: number) {
     basic.showLeds(`
         . . # . .
         . . # . .
@@ -72,9 +82,10 @@ function avanzar (velocidad: number, tiempo_ms: number) {
         . # # # .
         . . # . .
         `)
-    mover_temporal(xgo.direction_enum.Forward, velocidad, tiempo_ms)
+    mover_temporal(xgo.direction_enum.Forward, velocidad3, tiempo_ms3)
 }
-function retroceder (velocidad: number, tiempo_ms: number) {
+
+function retroceder(velocidad4: number, tiempo_ms4: number) {
     basic.showLeds(`
         . . # . .
         . # # # .
@@ -82,17 +93,19 @@ function retroceder (velocidad: number, tiempo_ms: number) {
         . . # . .
         . . # . .
         `)
-    mover_temporal(xgo.direction_enum.Backward, velocidad, tiempo_ms)
+    mover_temporal(xgo.direction_enum.Backward, velocidad4, tiempo_ms4)
 }
-input.onButtonPressed(Button.B, function () {
+
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
     toggle_gancho()
 })
-function girar_temporal (direccion: number, velocidad: number, tiempo_ms: number) {
-    xgo.rotate(direccion, velocidad)
-basic.pause(tiempo_ms)
-    xgo.rotate(direccion, 0)
+function girar_temporal(direccion2: number, velocidad5: number, tiempo_ms5: number) {
+    xgo.rotate(direccion2, velocidad5)
+    basic.pause(tiempo_ms5)
+    xgo.rotate(direccion2, 0)
 }
-function girar_derecha (velocidad: number, tiempo_ms: number) {
+
+function girar_derecha(velocidad6: number, tiempo_ms6: number) {
     basic.showLeds(`
         . . # . .
         . # . . .
@@ -100,8 +113,9 @@ function girar_derecha (velocidad: number, tiempo_ms: number) {
         . # . . .
         . . # . .
         `)
-    girar_temporal(xgo.rotate_enum.Right, velocidad, tiempo_ms)
+    girar_temporal(xgo.rotate_enum.Right, velocidad6, tiempo_ms6)
 }
+
 let clamp_state = 0
 let gancho_cerrado = 0
 basic.showLeds(`
@@ -112,10 +126,10 @@ basic.showLeds(`
     . # # # .
     `)
 gancho_cerrado = 200
-// =========================
-// INICIO DEL ROBOT
-// =========================
+//  =========================
+//  INICIO DEL ROBOT
+//  =========================
 xgo.init_xgo_serial(SerialPin.P14, SerialPin.P13)
 xgo.execution_action(xgo.action_enum.Default_posture)
-// Estado inicial del gancho
+//  Estado inicial del gancho
 abrir_gancho()
